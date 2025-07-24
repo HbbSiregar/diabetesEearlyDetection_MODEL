1b1aee417dfb5f6fd74ca75275b0de40fbc49acc
HbbSiregar <maruf.habibie.siregar@gmail.com>
2025-07-24 10:38:37 +0700

    Update

--- Deployment/eda.py
+++ Deployment/eda.py
@@ -270,8 +270,8 @@ def eda():
             padding: 0;
             margin: 0;
             ">
-            Dari bar diagram di atas saya ambil <b>insight</b> bahwa ternyata polyuria dan polydipsia menjadi salah dua gejala yang menandakan pasien positif diabetes.<br>
-            Polyuria (sering buang air kecil) dan polydipsia (haus berlebihan) terjadi karena tubuh mencoba mengeluarkan kelebihan glukosa melalui urin, yang menarik cairan dari jaringan.<br>
+            Dari bar diagram di atas saya ambil <b>insight</b> bahwa ternyata polyuria dan polydipsia menjadi salah dua gejala yang menandakan pasien positif diabetes.
+            Polyuria (sering buang air kecil) dan polydipsia (haus berlebihan) terjadi karena tubuh mencoba mengeluarkan kelebihan glukosa melalui urin, yang menarik cairan dari jaringan.
             Ini adalah gejala fisiologis paling awal dan khas pada diabetes, maka tidak mengherankan jika grafik menunjukkan dominasi keduanya.<br><br>
 
             Referensi : <a href="https://my.clevelandclinic.org/health/diseases/21945-diabetic-ketoacidosis-dka?utm" target="_blank" style="color:#1E90FF;">clevelandclinic : ketika orang punya sedikit insulin</a>
